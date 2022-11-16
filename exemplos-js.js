a4aea55500a6d186fef2c696c70c45b795d2b8b9
Alysson Oliveira <3oalysson@gmail.com>
2022-11-16 08:12:28 -0300

    Create exemplos-js.js

---
+++ exemplos-js.js
@@ -0,0 +1,20 @@
+<script type="text/javascript">
+/* Este é um script para cálculo de idade! */
+ 
+// Declara o ano atual para fazer o cálculo
+var anoAtual = 2014;
+ 
+// Pede que o usuário digite o ano em que nasceu
+var anoNascimento = prompt('Digite o ano em que você nasceu.');
+ 
+// Calcula a idade do usuário e armazena na variável idade
+var idade = anoAtual - anoNascimento;
+ 
+// Mostra ao usuário a idade que ele possui
+alert("Sua idade é: " + idade + " anos");
+</script>
+
+<script type="text/javascript">
+var nome = prompt('Digite seu nome: ');
+alert(nome + ', seja bem vindo!');
+</script>
