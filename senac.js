c604f6fb64a74b788b942338bd2481ed3299fad9
Alysson Oliveira <3oalysson@gmail.com>
2023-09-05 19:43:35 -0300

    Update senac.js

--- senac.js
+++ senac.js
@@ -5,3 +5,6 @@ var idadeDias = idade * 365;
 
 var idadeDaqui30Anos = (idadeDias + (30*365)) / 365;
 alert("Idade daqui 30 anos: " + idadeDaqui30Anos);
+
+var idadeDaqui40Meses = (idadeDias + (40 * 30)) / 365;
+alert("Idade daqui 40 meses: " + idadeDaqui40Meses);
