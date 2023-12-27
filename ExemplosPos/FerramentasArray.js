a47cc1bca5408d9e6030db4c51e47241d04582ad
Alysson <3oalysson@gmail.com>
2023-12-27 11:22:39 -0300

    Update FerramentasArray.js

--- ExemplosPos/FerramentasArray.js
+++ ExemplosPos/FerramentasArray.js
@@ -1,6 +1,12 @@
 const aprovados = ['Juca', 'Pedro', 'Carla', 'Paula'];
 
+console.log("Tradicional:")
 aprovados.forEach(function (nome, i, array) {
     console.log(`Nome aprovado: ${i+1}) ${nome}`);
-    console.log(array);
+    // console.log(array);
+})
+
+console.log("Arrow function:");
+aprovados.forEach((nome, i) => {
+    console.log(`Nome aprovado: ${i+1}) ${nome}`);
 })
