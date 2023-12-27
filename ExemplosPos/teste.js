0d970b2cb89cad44fedd7b8e0b0e3bab1ed2a4e7
Alysson <3oalysson@gmail.com>
2023-12-27 11:13:27 -0300

    Update teste.js

--- ExemplosPos/teste.js
+++ ExemplosPos/teste.js
@@ -8,3 +8,17 @@ console.log(valores[2]);
 
 valores[1] = 2;
 console.log(valores);
+
+valores.pop();
+valores.push(1);
+
+delete valores[2];
+
+valores.shift();
+valores.unshift(20);
+
+valores.splice(1, 0, 12, 22);
+valores.splice(1, 1, 2, 3);
+
+let novoValor = valores.slice(3);
+console.log(novoValor);
