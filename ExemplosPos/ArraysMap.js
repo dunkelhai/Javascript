9a81e1e80dda6d9ea7623d43fd01c141a03e712c
Alysson <3oalysson@gmail.com>
2023-12-27 13:54:27 -0300

    Update ArraysMap.js

--- ExemplosPos/ArraysMap.js
+++ ExemplosPos/ArraysMap.js
@@ -4,4 +4,10 @@ let resultado = numeros.map(function (e) {
     return e * 2;
 });
 
-console.log(resultado);
+const soma = el => el + 2;
+const triplo = el => el * 4;
+const moedaBRL = el => `R$ ${parseFloat(el).toFixed(2).replace('.', ',')}`;
+
+resultadoNovo = numeros.map(soma).map(triplo).map(moedaBRL);
+
+console.log(resultadoNovo);
