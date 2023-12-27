55fbd115311aa98b99de70e654141610bdabe875
Alysson <3oalysson@gmail.com>
2023-12-27 15:24:21 -0300

    Update testeReduce.js

--- ExemplosPos/testeReduce.js
+++ ExemplosPos/testeReduce.js
@@ -1,16 +1,19 @@
 // JSON
 const carrinho = [
-    { nome: "Batata", preco: 1.22 },
-    { nome: "Carne", preco: 4.42 },
-    { nome: "Cenoura", preco: 5.2 },
-    { nome: "Peixe", preco: 18.22 },
-    { nome: "Chocolate", preco: 8.22 }
+    { nome: "Batata", preco: 1.22, estoque: false},
+    { nome: "Carne", preco: 4.42 , estoque: false},
+    { nome: "Cenoura", preco: 5.2, estoque: false},
+    { nome: "Peixe", preco: 18.22, estoque: false},
+    { nome: "Chocolate", preco: 8.22, estoque: true}
 ];
 
+// 1
 const taxa = 5.88;
-
 const lista = carrinho.map(p => p.preco).reduce(function(inicial, atual){
     return inicial + atual;
 }, taxa);
-
 console.log(lista);
+
+// 2
+const temEstoque = (resultado, estoque) => resultado && estoque;
+console.log(carrinho.map(p => p.estoque).reduce(temEstoque))
