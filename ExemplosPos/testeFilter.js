941cfb330755e82663cbecb120861f0ba77490bc
Alysson <3oalysson@gmail.com>
2023-12-27 14:31:45 -0300

    Update testeFilter.js

--- ExemplosPos/testeFilter.js
+++ ExemplosPos/testeFilter.js
@@ -1,18 +1,14 @@
 // JSON
 const carrinho = [
-    '{ "nome": "Batata", "preco": 1.22 }',
-    '{ "nome": "Carne", "preco": 4.42 }',
-    '{ "nome": "Cenoura", "preco": 5.2 }',
-    '{ "nome": "Peixe", "preco": 18.22 }',
-    '{ "nome": "Chocolate", "preco": 8.22 }'
+    { nome: "Batata", preco: 1.22 },
+    { nome: "Carne", preco: 4.42 },
+    { nome: "Cenoura", preco: 5.2 },
+    { nome: "Peixe", preco: 18.22 },
+    { nome: "Chocolate", preco: 8.22 }
 ];
 
-const jsonParse = json => JSON.parse(json);
 const nome = produto => produto.nome;
-
-// const resultado = carrinho.map(jsonParse).map(nome);
-
 const caro = produto => produto.preco >= 5.00;
-const resultado = carrinho.map(jsonParse).filter(caro);
+const resultado = carrinho.filter(caro);
 
 console.log(resultado);
