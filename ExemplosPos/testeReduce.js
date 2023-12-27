a118fc25bd81515cc9d8e4cd286eff7574bc47e8
Alysson <3oalysson@gmail.com>
2023-12-27 15:11:27 -0300

    Create testeReduce.js

---
+++ ExemplosPos/testeReduce.js
@@ -0,0 +1,15 @@
+// JSON
+const carrinho = [
+    { nome: "Batata", preco: 1.22 },
+    { nome: "Carne", preco: 4.42 },
+    { nome: "Cenoura", preco: 5.2 },
+    { nome: "Peixe", preco: 18.22 },
+    { nome: "Chocolate", preco: 8.22 }
+];
+
+const lista = carrinho.map(p => p.preco).reduce(function(inicial, atual){
+    console.log(inicial, atual);
+    return inicial + atual;
+});
+
+console.log(lista);
