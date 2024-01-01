a9535494d92c71e0c628c45c40f5729bc6614680
Alysson <3oalysson@gmail.com>
2024-01-01 10:38:19 -0300

    Update DesestruturarTeste.js

--- ExemplosPos/DesestruturarTeste.js
+++ ExemplosPos/DesestruturarTeste.js
@@ -11,3 +11,12 @@ var valores = {maximo: 20, minimo: 10};
 console.log(
     intervalo(valores)
 );
+
+function intervalo2([minimo = 0, maximo = 100]){
+    const resultado = Math.random() * (maximo - minimo) + minimo;
+    return Math.floor(resultado);
+}
+
+console.log(
+    intervalo2([10, 20])
+)
